76a05f15ff03e9a6995cea235b79966c18164e13
mohammed riyas M <riyasx07@gmail.com>
2023-08-13 23:08:07 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/vgpt/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vgpt
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screts.dart
@@ -0,0 +1,1 @@
+const opeAIAPIKey='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';
